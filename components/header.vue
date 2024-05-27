8175c5d2bf8289e0da67b13cdb42ce84e22fa77b
info@28do.it <info@28do.it>
2024-05-27 12:42:39 +0200

    pulizia di gsap test

--- components/header.vue
+++ components/header.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="headerWrap flex gap-3 bg-slate-500 p-10">
     <Nav />
-    <div class="gbox w-4 h-4 bg-red-600"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
 //Utilizzo di Gsap
-import { onMounted } from "vue";
-import { gsap } from "gsap";
+// import { onMounted } from "vue";
+// import { gsap } from "gsap";
 
-onMounted(() => {
-  gsap.to(".gbox", {
-    duration: 1,
-    y: -20,
-    repeat: -1,
-    yoyo: true,
-  });
-});
+// onMounted(() => {
+//   gsap.to(".gbox", {
+//     duration: 1,
+//     y: -20,
+//     repeat: -1,
+//     yoyo: true,
+//   });
+// });
 //fine - Utilizzo di Gsap
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="sm:text-orange-500 md:text-slate-600 lg:text-lime-500">
     <p>Home</p>
-    <div class="box w-8 h-8 rounded bg-lime-600"></div>
+    <!-- <div class="box w-8 h-8 rounded bg-lime-600"></div> -->
   </div>
 </template>
 
 <script setup>
 //Utilizzo di Gsap
-import { onMounted } from "vue";
-import { gsap } from "gsap";
+// import { onMounted } from "vue";
+// import { gsap } from "gsap";
 
-onMounted(() => {
-  gsap.to(".box", {
-    duration: 3,
-    rotation: 360,
-    scale: 2,
-    repeat: -1,
-    yoyo: true,
-  });
-});
+// onMounted(() => {
+//   gsap.to(".box", {
+//     duration: 3,
+//     rotation: 360,
+//     scale: 2,
+//     repeat: -1,
+//     yoyo: true,
+//   });
+// });
 //fine - Utilizzo di Gsap
 </script>
 
