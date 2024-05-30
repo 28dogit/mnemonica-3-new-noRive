1ec8185a73f3bae2b8bd4728de155cf0c4957906
info@28do.it <info@28do.it>
2024-05-30 18:07:07 +0200

    lavorazione sui link degli approfondimenti, integrazione nuove icone non funzionante da rivedere!!!

--- pages/focusTransition.vue
+++ pages/focusTransition.vue
@@ -3,13 +3,23 @@
     <h1>Much More Than Screening</h1>
 
     <h3>PLAY</h3>
-    <p>Touch Day and Bin, and your playlist is ready to watch.</p>
+    <p>
+      Touch Day and Bin, and your is
+      <UChip color="seagull">ready</UChip> to watch.
+    </p>
 
     <h3>MEET</h3>
     <p>
       Review, comment, approve, import into your
-      <a ref="button" @click="showOverlay($event)" class="open-btn underline cursor-pointer text-blue-400">NLEs</a>,
-      stay up to date.
+      <a
+        ref="button"
+        @click="showOverlay($event)"
+        class="open-btn underline cursor-pointer text-seagull-400"
+        >NLEs</a
+      ><UIcon name="i-heroicons-light-bulb-solid" class="text-chenin-400"></UIcon>, stay
+      <!-- <UIcon name="i-mdi-account-file" class="text-chenin-400"></UIcon>
+      <UIcon name="i-lucide-arrow-big-right" class="text-chenin-400"></UIcon> -->
+      up to date.
     </p>
 
     <h3>PROTECT</h3>
@@ -18,7 +28,7 @@
       <a
         ref="button"
         @click="showOverlay($event)"
-        class="open-btn underline cursor-pointer text-blue-400"
+        class="open-btn underline cursor-pointer text-seagull-400"
         >watermark</a
       >.
     </p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+// const { iconsPlugin, getIconCollections } = require("@egoist/tailwindcss-icons")
 
 export default <Partial<Config>>{
   darkMode: 'class',
@@ -12,6 +13,19 @@ export default <Partial<Config>>{
       "./app.vue",
     // Aggiungi altri percorsi se necessario
   ],
+  // module: {
+  //   plugins: [
+  //     iconsPlugin({
+  //       // Select the icon collections you want to use
+  //       // You can also ignore this option to automatically discover all individual icon packages you have installed
+  //       // If you install @iconify/json, you should explicitly specify the collections you want to use, like this:
+  //       collections: getIconCollections(["mdi", "lucide"]),
+  //       // If you want to use all icons from @iconify/json, you can do this:
+  //       // collections: getIconCollections("all"),
+  //       // and the more recommended way is to use `dynamicIconsPlugin`, see below.
+  //     }),
+  //   ],
+  // },
   theme: {
     extend: {
       aspectRatio: {
@@ -20,7 +34,6 @@ export default <Partial<Config>>{
         video: '16 / 9'
       },
       colors:{
-        mne_base:'#5DC9EA',
         'seagull': {
             '50': '#f1fafe',
             '100': '#e3f3fb',
