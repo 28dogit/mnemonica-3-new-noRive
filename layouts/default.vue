f148ca626049fa1434159156477f4120e123e87c
info@28do.it <info@28do.it>
2024-11-16 17:36:38 +0100

    Test con ghirlanda e blend mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="mainContent" class="min-h-screen flex flex-col flex-grow">
     <div class="bbg"></div>
+    <div class="testghi">
+      <div class="ghirlanda-updx">
+        <img src="/assets/img/Ghirlanda-up-dx-600.png" alt="" />
+      </div>
+      <div class="ghirlanda-updx test1">
+        <img src="/assets/img/Ghirlanda-up-dx-600.png" alt="" />
+      </div>
+      <div class="ghirlanda-updx test2">
+        <img src="/assets/img/Ghirlanda-up-dx-600.png" alt="" />
+      </div>
+    </div>
     <Header />
     <slot class="min-h-screen" />
     <Footer />
@@ -11,4 +22,20 @@
 
 <style>
 /* w-screen h-screen bg-slate-300 dark:bg-woodsmoke-950 */
+.testghi {
+  display: flex;
+}
+.ghirlanda-updx {
+  transform: rotate(-20deg);
+  max-width: 300px;
+  mix-blend-mode: luminosity;
+  float: left;
+  opacity: 0.6;
+}
+.ghirlanda-updx.test1 {
+  mix-blend-mode: color-dodge;
+}
+.ghirlanda-updx.test2 {
+  mix-blend-mode: screen;
+}
 </style>
