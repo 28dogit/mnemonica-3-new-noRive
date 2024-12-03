a1cb2fff1e280766a4cf3997f6f685230d55dc12
28dogit <info@28do.it>
2024-12-03 10:16:57 +0100

    ottimizzazione ghirlande

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,10 +4,10 @@
 
     <div id="ghirlandeContainer" class="w-screen h-screen absolute top-0 left-0 -z-10">
       <div class="ghirlanda-updx l1" data-speed="0.4">
-        <NuxtImg src="/assets/img/Ghirlanda-web-up-dx.png" densities="x1" alt="" />
+        <NuxtImg src="/assets/img/Ghirlanda-web-up-dx-500.png" densities="x1" alt="" />
       </div>
       <div class="ghirlanda-dwsx l1" data-speed="0.6">
-        <NuxtImg src="/assets/img/Ghirlanda-web-dw-sx.png" densities="x1" alt="" />
+        <NuxtImg src="/assets/img/Ghirlanda-web-dw-sx-500.png" densities="x1" alt="" />
       </div>
     </div>
 
