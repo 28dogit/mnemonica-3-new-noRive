584ca549f2c790ee22b2d31339f501a035fea7ef
info@28do.it <info@28do.it>
2024-12-15 17:41:08 +0100

    Struttura a blocchi nuova onepage

---
+++ pages/index_2.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <div id="hero-section">
+      <div id="ghirlanda-element">
+        <svg viewBox="0 0 100 100" class="circle-place-holder">
+          <circle cx="50" cy="50" r="50"></circle>
+        </svg>
+      </div>
+      <div id="hero-content-wrapper">
+        <div id="hero-content"><p>HERO</p></div>
+      </div>
+    </div>
+    <div id="modules-section">
+      <div id="modules-element">
+        <svg viewBox="0 0 100 100" class="circle-place-holder">
+          <circle cx="50" cy="50" r="50"></circle>
+        </svg>
+      </div>
+      <div id="modules-content-wrapper">
+        <div id="modules-content"><p>MODULES</p></div>
+      </div>
+    </div>
+    <div id="phases-section">
+      <div id="phases-element">
+        <svg viewBox="0 0 100 100" class="circle-place-holder">
+          <circle cx="50" cy="50" r="50"></circle>
+        </svg>
+      </div>
+      <div id="phases-content-wrapper">
+        <div id="phases-content"><p>PHASES</p></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
