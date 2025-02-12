5f4d787ed8f3150c8012ef40ef41a2c1791ae759
info@28do.it <info@28do.it>
2025-02-12 23:42:57 +0100

    Aggiornamento del componente focusBTN.vue con un nuovo canvas per l'animazione; rimozione del codice obsoleto e aggiornamento del file focusbtn.riv.

--- components/focusBTN.vue
+++ components/focusBTN.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="focusWrapper">
-    <span class="focusTxt">Focus</span>
-    <span class="focusBtn">+</span>
+    <!-- <span class="focusTxt">Focus</span>
+    <span class="focusBtn">+</span> -->
+    <canvas
+      ref="canvasRefBtn"
+      id="canvasFocusBtn"
+      style="position: absolute; height: 70px; width: 70px"
+    ></canvas>
   </div>
-  <canvas
-    ref="canvasRefBtn"
-    id="canvasFocusBtn"
-    style="position: absolute; height: 105px; width: 100px; z-index: 5"
-  ></canvas>
 </template>
 <script setup>
 import { onMounted, ref } from "vue";
@@ -50,33 +50,35 @@ onMounted(() => {
 @use "@/assets/css/_globals.scss" as *;
 @use "@/assets/css/_breakpoints.scss" as bp;
 .focusWrapper {
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  gap: 1rem;
-  height: 30px;
-  border-radius: 30px;
-  width: fit-content;
-  min-width: 120px;
-  padding-left: 1rem;
-  margin-top: 1rem;
-  background-color: $gradient_2;
-  .focusTxt {
-    opacity: 1;
-    //display: none;
-  }
-  .focusBtn {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 1.5rem;
-    // color: $mne_secondary;
-    color: $mne_text-on-dark;
-    margin: 0;
-    background-color: $gradient_4;
-    width: 30px;
-    height: 30px;
-    border-radius: 15px;
-  }
+  height: 70px;
+  width: 70px;
+  // display: flex;
+  // align-items: center;
+  // justify-content: flex-end;
+  // gap: 1rem;
+  // height: 30px;
+  // border-radius: 30px;
+  // width: fit-content;
+  // min-width: 120px;
+  // padding-left: 1rem;
+  // margin-top: 1rem;
+  // background-color: $gradient_2;
+  // .focusTxt {
+  //   opacity: 1;
+  //   //display: none;
+  // }
+  // .focusBtn {
+  //   display: flex;
+  //   align-items: center;
+  //   justify-content: center;
+  //   font-size: 1.5rem;
+  //   // color: $mne_secondary;
+  //   color: $mne_text-on-dark;
+  //   margin: 0;
+  //   background-color: $gradient_4;
+  //   width: 30px;
+  //   height: 30px;
+  //   border-radius: 15px;
+  // }
 }
 </style>
