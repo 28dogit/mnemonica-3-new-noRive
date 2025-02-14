b8f46e942fe74f6693e4901840d3b89cfd691fe3
info@28do.it <info@28do.it>
2025-02-14 13:34:07 +0100

    Aggiornamento del componente focusBTN.vue con modifica dell'artboard in "Focus 3" e gestione degli eventi Rive tramite switch case. Aggiornamento del file focusbtn.riv.

--- components/focusBTN.vue
+++ components/focusBTN.vue
@@ -19,7 +19,7 @@ let rFocusBtn = null; // Istanza di Rive
 onMounted(() => {
   rFocusBtn = new Rive({
     src: "/assets/rive/focusbtn.riv",
-    artboard: "Focus 2",
+    artboard: "Focus 3",
     canvas: canvasRefBtn.value,
     autoplay: true,
     stateMachines: "FocusBtn_animation",
@@ -35,15 +35,25 @@ onMounted(() => {
 
   function onRiveEventReceived(riveEvent) {
     const eventData = riveEvent.data;
-    //const eventProperties = eventData.properties;
-    if (eventData.type === RiveEventType.General) {
-      emit("click"); // Per il desktop
-      emit("touch"); // Per dispositivi mobili
-      emit("pointerdown");
-      console.log("Event name", eventData.name);
-    } else if (eventData.type === RiveEventType.OpenUrl) {
-      window.open(eventData.url);
+    switch (eventData.name) {
+      case "Event28":
+        console.log("Event da switch case", eventData.name);
+        emit("click");
+        emit("touch");
+        emit("pointerdown");
+        break;
+      default:
+        break;
     }
+    //const eventProperties = eventData.properties;
+    // if (eventData.type === RiveEventType.General) {
+    //   emit("click"); // Per il desktop
+    //   emit("touch"); // Per dispositivi mobili
+    //   emit("pointerdown");
+    //   console.log("Event name", eventData.name);
+    // } else if (eventData.type === RiveEventType.OpenUrl) {
+    //   window.open(eventData.url);
+    // }
   }
   // Add event listener and provide callback to handle Rive Event
   rFocusBtn.on(EventType.RiveEvent, onRiveEventReceived);
