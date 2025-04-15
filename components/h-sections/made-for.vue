d4d9ae448aa5d189cfe679768d6924855285dd95
28dogit <info@28do.it>
2025-04-15 18:05:39 +0200

    aggiornamenti modali

--- components/h-sections/made-for.vue
+++ components/h-sections/made-for.vue
@@ -2,16 +2,27 @@
   <div class="section-made-for">
     <h1>made-for</h1>
     <div class="content">
-      <div class="boxMF">Producers</div>
+      <div class="boxMF">
+        <BtnMaster @click="(isModalOpen = true), (modalId = 'producers')"
+          >Producers</BtnMaster
+        >
+      </div>
       <div class="boxMF">Post Supervisors</div>
       <div class="boxMF">Post-production Labs</div>
       <div class="boxMF">Distributors</div>
       <div class="boxMF">Broadcasters / OTT</div>
     </div>
   </div>
+  <ModalsModale50 :isOpen="isModalOpen" :isModal="modalId" @close="closeModal" />
 </template>
 
-<script setup></script>
+<script setup>
+const isModalOpen = ref(false);
+const modalId = ref(null); // qui indico il nome del modale che devo aprire in content/modali50
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+</script>
 
 <style lang="scss" scoped>
 .section-made-for {
